437cde1dedd103664e9059b034c1e26a84ee52a6
Rohan Morris <sirrom2005@gmail.com>
2025-02-09 23:09:29 -0500

    update common text

--- lib/Common/CommonText.dart
+++ lib/Common/CommonText.dart
@@ -4,23 +4,13 @@ class CommonText{
   static const double borderRadius = 30.0;
   static const double appTabHeight = 60.0;
   static const double appMargin = 8.0;
+  static const String ipoProText = 'To add you signature, please enter the code provided\nearlier. To upload your signature, after signing on the next screen, press the Submit button.';
 
   static const int serverTimeOutInSeconds = 30;
   static const String errorResponse = 'Invalid response';
   static const String serverError = 'Error connecting to server';
   static const String serverTimeOut = 'Opps!! Server Timeout';
-  static const String locationDisabled = 'Your location service is disabled\n\nWe require your device’s location solely to determine your eligibility to access geo-restricted content. Location data is used only to comply with licensing agreements and ensure that content is available in your region.';
-  static const String locationNotFoundByDevice = 'Unable to retrieve device location.\n\nWe require your device’s location solely to determine your eligibility to access geo-restricted content. Location data is used only to comply with licensing agreements and ensure that content is available in your region.';
-  static const double appSideMargin = 20;
-  static const double imageMargin = 8;
+
   static const String loginTokenKey = "LOGIN_TOKEN_KEY";
-  static const String audioQuality = "AUDIO_QUALITY";
-  static const String maxSessionKey = "MAX_SESSION_KEY";
-  static const String selectedAudioQuality = "SELECTED_AUDIO_QUALITY";
-  static const String imageSeparator = "***";
   static const String maxSession = 'Please logout of a existing\ndevice to continue';
-  static const String passwordText = "Your password must contain at least 8 characters, including a minimum of one lowercase letter, an uppercase letter, a unique character and a number";
-  static const String signUp = 'Sign up';
-
-
 }
