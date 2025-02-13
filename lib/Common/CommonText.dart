1a65e607f528be25ce9bee1c1795031516e230a1
Rohan Morris <sirrom2005@gmail.com>
2025-02-12 19:52:37 -0500

    add loan text

--- lib/Common/CommonText.dart
+++ lib/Common/CommonText.dart
@@ -5,6 +5,7 @@ class CommonText{
   static const double appTabHeight = 60.0;
   static const double appMargin = 8.0;
   static const String ipoProText = 'To add you signature, please enter the code provided\nearlier. To upload your signature, after signing on the next screen, press the Submit button.';
+  static const String noLoan = 'You have no loan with us at this time.\n Please contact your Portfolio Advisor if you\nwould like to discuss our loan offerings';
 
   static const int serverTimeOutInSeconds = 30;
   static const String errorResponse = 'Invalid response';
